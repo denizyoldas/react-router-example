a6b7fd59d3cadeecba688a9c5c79c75fe1b07ce3
Deniz.Aksu <deniz.aksu@dijitalkurye.com.tr>
2021-12-12 22:03:07 +0300

    first commit

---
+++ src/components/quotes/QuoteItem.js
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Link } from "react-router-dom";
+import classes from "./QuoteItem.module.css";
+
+const QuoteItem = (props) => {
+  return (
+    <li className={classes.item}>
+      <figure>
+        <blockquote>
+          <p>{props.text}</p>
+        </blockquote>
+        <figcaption>{props.author}</figcaption>
+      </figure>
+      <Link to={`/quotes/${props.id}`} className="btn">
+        View Fullscreen
+      </Link>
+    </li>
+  );
+};
+
+export default QuoteItem;
---
+++ src/components/quotes/QuoteList.js
@@ -0,0 +1,52 @@
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
+const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAsc = queryParams.get("sort") === "asc";
+
+  const shortQuotes = sortQuotes(props.quotes, isSortingAsc);
+
+  const changeSortHandler = () => {
+    history.push(`${location.pathname}?sort=${isSortingAsc ? "desc" : "asc"}`);
+  };
+
+  return (
+    <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortHandler}>
+          Sort {isSortingAsc ? "Ascending" : "Descending"}
+        </button>
+      </div>
+      <ul className={classes.list}>
+        {shortQuotes.map((quote) => (
+          <QuoteItem
+            key={quote.id}
+            id={quote.id}
+            author={quote.author}
+            text={quote.text}
+          />
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
+
+export default QuoteList;
